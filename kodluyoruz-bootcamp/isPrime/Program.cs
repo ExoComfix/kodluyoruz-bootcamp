e6bfaacdf2300f9ad95acd7b36bf69fd1c5053ae
Bugra Ozturk <oozturkks@gmail.com>
2022-03-27 12:26:14 +0300

    first commit

---
+++ kodluyoruz-bootcamp/isPrime/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace isPrime
+{
+    class Program
+    {
+        static void isPrime(string[] args)
+        {
+            int number = 0;
+            bool isPrime = true;
+            Console.Write("Please Enter Number:");
+            try
+            {
+                number = Convert.ToInt16(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter digits.");
+            };
+            
+            for (int i = 2; i < Math.Sqrt(number); i++)
+            {
+                if(number%i == 0)
+                    prime = false;
+            }
+
+            if (number == 1)
+                prime = false;
+
+            if (prime)
+                Console.WriteLine("Prime!");
+            else
+                Console.WriteLine("Not prime!");
+        }
+    }
+}
